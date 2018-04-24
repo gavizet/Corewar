a4b5ec32b7d43ba9c2d0d4157ce4b6419bf994aa
Gaetan VIZET <gavizet@e2r11p3.42.fr>
2018-04-24 18:57:38 +0200

    champions.s

---
+++ champion.s
@@ -0,0 +1,39 @@
+.name		"Hitler la maman de Arthur"
+.comment 	"Aime la mettre en anal a Bob"
+
+winter_is_coming:
+	live	%42
+	ld		%12345678, r2
+	st		r2, -44
+	st		r2, -49
+	st		r2, -54
+	st		r2, -59
+	st		r2, -64
+	st		r2, -69
+	st		r2, -74
+	st		r2, -79
+	st		r2, -84
+	st		r2, -89
+	st		r2, -94
+	st		r2, -99
+	st		r2, -104
+	st		r2, -109
+	st		r2, -114
+	st		r2, -119
+	st		r2, -124
+	st		r2, -129
+	st		r2, -134
+	st		r2, -139
+	st		r2, -144
+	st		r2, -149
+	st		r2, -154
+	st		r2, -159
+	st		r2, -164
+	st		r2, -169
+	st		r2, -174
+	st		r2, -179
+	st		r2, -184
+	st		r2, -189
+	st		r2, -194
+	st		r2, -199
+	zjmp	%:winter_is_coming
